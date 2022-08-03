a91a77525559c4a871e8155a5bd571f7ad1de3d9
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-08-03 15:13:23 +0200

    Regenerated sample keys

--- generate.py
+++ generate.py
@@ -4,22 +4,24 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 PRIVATE_PATH = 'keys/private.key'
 PUBLIC_PATH = 'keys/public.key'
 
+# Private key
 private = rsa.generate_private_key(65537, 2048)
-public = private.public_key()
-
-pub_pem = public.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
-
-with open(PUBLIC_PATH, 'wb') as f:
-    f.write(pub_pem)
-
-prv_pem = private.private_bytes(
+private_pem = private.private_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PrivateFormat.TraditionalOpenSSL,
     encryption_algorithm=serialization.NoEncryption()
 )
 
 with open(PRIVATE_PATH, 'wb') as f:
-    f.write(prv_pem)
+    f.write(private_pem)
+
+
+# Public key
+public = private.public_key()
+public_pem = public.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+with open(PUBLIC_PATH, 'wb') as f:
+    f.write(public_pem)
