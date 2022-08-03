faa43e4ee090c2770d75c5dd87fb385f623c76b8
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-08-03 16:32:33 +0200

    Added cli options to generate command

--- generate.py
+++ generate.py
@@ -1,8 +1,13 @@
+import sys
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 PRIVATE_PATH = 'keys/private.key'
 PUBLIC_PATH = 'keys/public.key'
+
+if len(sys.argv) > 2:
+    PRIVATE_PATH = sys.argv[1]
+    PUBLIC_PATH = sys.argv[2]
 
 # Private key
 private = rsa.generate_private_key(65537, 2048)
